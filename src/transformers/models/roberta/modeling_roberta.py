61fa94d866c0c93d8491f603bdbb2bb3d723ec97
GY-Jeong <jyjg1124@gmail.com>
2021-10-28 14:30:33 +0900

    클래스 인덱스 인코딩

--- src/transformers/models/roberta/modeling_roberta.py
+++ src/transformers/models/roberta/modeling_roberta.py
@@ -77,14 +77,14 @@ class RobertaEmbeddings(nn.Module):
         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
-        # self.token_label_type_embeddings = nn.Embedding(8, config.hidden_size)
-        self.token_label_0_type_embeddings = nn.Embedding(2, config.hidden_size)
-        self.token_label_1_type_embeddings = nn.Embedding(2, config.hidden_size)
-        self.token_label_2_type_embeddings = nn.Embedding(2, config.hidden_size)
-        self.token_label_3_type_embeddings = nn.Embedding(2, config.hidden_size)
-        self.token_label_4_type_embeddings = nn.Embedding(2, config.hidden_size)
-        self.token_label_5_type_embeddings = nn.Embedding(2, config.hidden_size)
-        self.token_label_6_type_embeddings = nn.Embedding(2, config.hidden_size)
+        self.token_label_type_embeddings = nn.Embedding(8, config.hidden_size)
+        # self.token_label_0_type_embeddings = nn.Embedding(2, config.hidden_size)
+        # self.token_label_1_type_embeddings = nn.Embedding(2, config.hidden_size)
+        # self.token_label_2_type_embeddings = nn.Embedding(2, config.hidden_size)
+        # self.token_label_3_type_embeddings = nn.Embedding(2, config.hidden_size)
+        # self.token_label_4_type_embeddings = nn.Embedding(2, config.hidden_size)
+        # self.token_label_5_type_embeddings = nn.Embedding(2, config.hidden_size)
+        # self.token_label_6_type_embeddings = nn.Embedding(2, config.hidden_size)
 
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
@@ -107,15 +107,16 @@ class RobertaEmbeddings(nn.Module):
         )
 
     def forward(
-            # self, input_ids=None, token_type_ids=None, token_label_type_ids=None, position_ids=None, inputs_embeds=None, past_key_values_length=0
-            self, input_ids=None, token_type_ids=None,
-            token_label_0_type_ids=None,
-            token_label_1_type_ids=None,
-            token_label_2_type_ids=None,
-            token_label_3_type_ids=None,
-            token_label_4_type_ids=None,
-            token_label_5_type_ids=None,
-            token_label_6_type_ids=None, position_ids=None, inputs_embeds=None, past_key_values_length=0
+            self, input_ids=None, token_type_ids=None, token_label_type_ids=None, position_ids=None, inputs_embeds=None, past_key_values_length=0
+            # self, input_ids=None, token_type_ids=None,
+            # # token_label_0_type_ids=None,
+            # # token_label_1_type_ids=None,
+            # # token_label_2_type_ids=None,
+            # # token_label_3_type_ids=None,
+            # # token_label_4_type_ids=None,
+            # # token_label_5_type_ids=None,
+            # # token_label_6_type_ids=None, 
+            # position_ids=None, inputs_embeds=None, past_key_values_length=0
     ):
         if position_ids is None:
             if input_ids is not None:
@@ -146,20 +147,20 @@ class RobertaEmbeddings(nn.Module):
             inputs_embeds = self.word_embeddings(input_ids)
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
-        # if token_label_type_ids is None:
-        #     embeddings = inputs_embeds + token_type_embeddings
-        # else:
-        #     token_label_type_embeddings = self.token_label_type_embeddings(token_label_type_ids)
-        #     embeddings = inputs_embeds + token_type_embeddings + token_label_type_embeddings
+        if token_label_type_ids is None:
+            embeddings = inputs_embeds + token_type_embeddings
+        else:
+            token_label_type_embeddings = self.token_label_type_embeddings(token_label_type_ids)
+            embeddings = inputs_embeds + token_type_embeddings + token_label_type_embeddings
 
-        embeddings = inputs_embeds + token_type_embeddings + \
-                     self.token_label_0_type_embeddings(token_label_0_type_ids) + \
-                     self.token_label_1_type_embeddings(token_label_1_type_ids) + \
-                     self.token_label_2_type_embeddings(token_label_2_type_ids) + \
-                     self.token_label_3_type_embeddings(token_label_3_type_ids) + \
-                     self.token_label_4_type_embeddings(token_label_4_type_ids) + \
-                     self.token_label_5_type_embeddings(token_label_5_type_ids) + \
-                     self.token_label_6_type_embeddings(token_label_6_type_ids)
+        # embeddings = inputs_embeds + token_type_embeddings + \
+        #              self.token_label_0_type_embeddings(token_label_0_type_ids) + \
+        #              self.token_label_1_type_embeddings(token_label_1_type_ids) + \
+        #              self.token_label_2_type_embeddings(token_label_2_type_ids) + \
+        #              self.token_label_3_type_embeddings(token_label_3_type_ids) + \
+        #              self.token_label_4_type_embeddings(token_label_4_type_ids) + \
+        #              self.token_label_5_type_embeddings(token_label_5_type_ids) + \
+        #              self.token_label_6_type_embeddings(token_label_6_type_ids)
 
         # embeddings = inputs_embeds + token_type_embeddings + token_label_type_embeddings
         if self.position_embedding_type == "absolute":
@@ -773,14 +774,14 @@ class RobertaModel(RobertaPreTrainedModel):
             input_ids=None,
             attention_mask=None,
             token_type_ids=None,
-            # token_label_type_ids=None,
-            token_label_0_type_ids=None,
-            token_label_1_type_ids=None,
-            token_label_2_type_ids=None,
-            token_label_3_type_ids=None,
-            token_label_4_type_ids=None,
-            token_label_5_type_ids=None,
-            token_label_6_type_ids=None,
+            token_label_type_ids=None,
+            # token_label_0_type_ids=None,
+            # token_label_1_type_ids=None,
+            # token_label_2_type_ids=None,
+            # token_label_3_type_ids=None,
+            # token_label_4_type_ids=None,
+            # token_label_5_type_ids=None,
+            # token_label_6_type_ids=None,
             position_ids=None,
             head_mask=None,
             inputs_embeds=None,
@@ -876,14 +877,14 @@ class RobertaModel(RobertaPreTrainedModel):
             input_ids=input_ids,
             position_ids=position_ids,
             token_type_ids=token_type_ids,
-            # token_label_type_ids=token_label_type_ids,
-            token_label_0_type_ids=token_label_0_type_ids,
-            token_label_1_type_ids=token_label_1_type_ids,
-            token_label_2_type_ids=token_label_2_type_ids,
-            token_label_3_type_ids=token_label_3_type_ids,
-            token_label_4_type_ids=token_label_4_type_ids,
-            token_label_5_type_ids=token_label_5_type_ids,
-            token_label_6_type_ids=token_label_6_type_ids,
+            token_label_type_ids=token_label_type_ids,
+            # token_label_0_type_ids=token_label_0_type_ids,
+            # token_label_1_type_ids=token_label_1_type_ids,
+            # token_label_2_type_ids=token_label_2_type_ids,
+            # token_label_3_type_ids=token_label_3_type_ids,
+            # token_label_4_type_ids=token_label_4_type_ids,
+            # token_label_5_type_ids=token_label_5_type_ids,
+            # token_label_6_type_ids=token_label_6_type_ids,
             inputs_embeds=inputs_embeds,
             past_key_values_length=past_key_values_length,
         )
@@ -1220,13 +1221,13 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
             attention_mask=None,
             token_type_ids=None,
             token_label_type_ids=None,
-            token_label_0_type_ids=None,
-            token_label_1_type_ids=None,
-            token_label_2_type_ids=None,
-            token_label_3_type_ids=None,
-            token_label_4_type_ids=None,
-            token_label_5_type_ids=None,
-            token_label_6_type_ids=None,
+            # token_label_0_type_ids=None,
+            # token_label_1_type_ids=None,
+            # token_label_2_type_ids=None,
+            # token_label_3_type_ids=None,
+            # token_label_4_type_ids=None,
+            # token_label_5_type_ids=None,
+            # token_label_6_type_ids=None,
             position_ids=None,
             head_mask=None,
             inputs_embeds=None,
@@ -1247,14 +1248,14 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
             input_ids,
             attention_mask=attention_mask,
             token_type_ids=token_type_ids,
-            # token_label_type_ids=token_label_type_ids,
-            token_label_0_type_ids=token_label_0_type_ids,
-            token_label_1_type_ids=token_label_1_type_ids,
-            token_label_2_type_ids=token_label_2_type_ids,
-            token_label_3_type_ids=token_label_3_type_ids,
-            token_label_4_type_ids=token_label_4_type_ids,
-            token_label_5_type_ids=token_label_5_type_ids,
-            token_label_6_type_ids=token_label_6_type_ids,
+            token_label_type_ids=token_label_type_ids,
+            # token_label_0_type_ids=token_label_0_type_ids,
+            # token_label_1_type_ids=token_label_1_type_ids,
+            # token_label_2_type_ids=token_label_2_type_ids,
+            # token_label_3_type_ids=token_label_3_type_ids,
+            # token_label_4_type_ids=token_label_4_type_ids,
+            # token_label_5_type_ids=token_label_5_type_ids,
+            # token_label_6_type_ids=token_label_6_type_ids,
             position_ids=position_ids,
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
